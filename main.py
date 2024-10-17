9ffe80197071785262d5a3a5c515df543a73b63a
j-m-a-g <jmaghamez@gmail.com>
2024-10-17 14:48:28 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+# https://docs.roboflow.com/deploy/hosted-api/custom-models/object-detection
+
+from tkinter import *
+from tkinter import ttk
+
 from roboflow import Roboflow
 
-roboflowObject = Roboflow(api_key="xiu3cjOBKOMlP2zSBvTr")
+roboflowObject = Roboflow(api_key = "xiu3cjOBKOMlP2zSBvTr")
 project = roboflowObject.workspace().project("howoldismyplant")
 model = project.version(1).model
 
-# infer on a local image
-print(model.predict("C:/Users/Joseph/Downloads/money-tree-plant-royalty-free-image-1726508639.jpg", confidence=1, overlap=30).json())
+# Initialize and create a tkinter window for the user-interface
+mainWindow = Tk()
+mainWindow.title("How Old Is My Plant?")
+mainWindow.geometry("300x300")
 
-# visualize your prediction
-model.predict("C:/Users/Joseph/Downloads/money-tree-plant-royalty-free-image-1726508639.jpg", confidence=1, overlap=30).save("prediction.jpg")
+# Use inference to print out all_properties from a local image
+print(model.predict("https://dam.thdstatic.com/content/production/lh2loaHOQFynCwhJ2EIOUg/Y6kklzWdiWY43H2fqRQKQg/Original%20file/2023_1v2_Low_Maintenance_Indoor.jpg", hosted = True, confidence = 1, overlap = 30).json())
 
-# infer on an image hosted elsewhere
+# Create a .jpg image to visually see resulting predictions
+model.predict("https://dam.thdstatic.com/content/production/lh2loaHOQFynCwhJ2EIOUg/Y6kklzWdiWY43H2fqRQKQg/Original%20file/2023_1v2_Low_Maintenance_Indoor.jpg", hosted = True, confidence = 1, overlap = 30).save("prediction.jpg")
+
+print()
+print()
+print(model.predict.__class__)
+
+# Infer on a hosted image
 # print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
