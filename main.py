a5a07799402c41fb9fa192c41f3960d6cd4abd60
j-m-a-g <jmaghamez@gmail.com>
2024-10-18 13:54:13 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
-from tkinter import *
-from tkinter import ttk
 from roboflow import Roboflow
 
 # Initialize Roboflow project
 rf = Roboflow(api_key="xiu3cjOBKOMlP2zSBvTr")
 project = rf.workspace("fx-coding-club-1armu").project("howoldismyplant")
 model = project.version(1).model
+    
+# Display content to enter either a local image path or an online image URL
+print("\nHow Old Is My Plant?")
 
-# Initialize tkinter window for the user-interface
-mainWindow = Tk()
-mainWindow.title("How Old Is My Plant?")
-mainWindow.geometry("300x300")
-predictions_list = []
+isHostedOption = input("\nLocal [l/local] or Hosted [h/hosted]?: ")
+if isHostedOption == "l":
+    imagePathInput = input("\nLocal Image Path: ")
+    isHostedBoolean = False
+elif isHostedOption == "local":
+    imagePathInput = input("\nLocal Image Path: ")
+    isHostedBoolean = False
+elif isHostedOption == "h":
+    imagePathInput = input("\nOnline Image URL: ")
+    isHostedBoolean = True
+elif isHostedOption == "hosted":
+    imagePathInput = input("\nOnline Image URL: ")
+    isHostedBoolean = True
+else:
+    print("\nOops! Try Again :(")
 
 # Use inference to predict on a hosted image
-<<<<<<< HEAD
-image_url = "https://cdn.vectorstock.com/i/1000v/81/68/parts-of-a-plant-vector-1858168.jpg"
-#image_url = "https://reefertilizer.com/wp-content/uploads/2018/12/IMG_20181219_185434-scaled-e1596134064368.jpg"
-=======
-# image_url = "https://cdn.vectorstock.com/i/1000v/81/68/parts-of-a-plant-vector-1858168.jpg"
-image_url = "https://www.bhg.com/thmb/SfvVALaQxFyi4vYdbhBR11S41S8=/1280x0/filters:no_upscale():strip_icc()/indoor-potted-houseplants-703b321a-81cf8e1f9aee48a28e1be3bbc45e4386.jpg"
->>>>>>> cf39b950e8059d9218cc0ff483b4fe70bd7bf1b0
-prediction_result = model.predict(image_url, hosted=True, confidence=1, overlap=30).json()
+prediction_result = model.predict(imagePathInput, hosted = isHostedBoolean, confidence = 1, overlap = 30).json()
 
-# Create and save a prediction image to visually see resulting predictions
-model.predict(image_url, hosted=True, confidence=1, overlap=30).save("prediction.jpg")
+# Option of whether to save an individual image that visually displays the predictions derived from the model
+savePredictionsResultImage = input("Save the model's predictions as a separate image? [y/yes] [n/no]: ")
+if savePredictionsResultImage == "y" or "yes":
+    # Create and save a prediction image to visually see resulting predictions
+    model.predict(imagePathInput, hosted = isHostedBoolean, confidence = 1, overlap = 30).save("predictions.jpg")
 
 # Extract values from the prediction result
 key_list = []
@@ -45,11 +52,7 @@ for item in value_list:
         flat_list.append(item)
 
 # Print flattened list
-<<<<<<< HEAD
 print("\nFlattened Values:", flat_list)
-=======
-#print("Flattened Values:", flat_list)
->>>>>>> cf39b950e8059d9218cc0ff483b4fe70bd7bf1b0
 
 # Function to create the data structure from the prediction results
 def create_data_structure(prediction_result_param):
@@ -85,103 +88,22 @@ data = create_data_structure(prediction_result)
 keys = []
 values = []
 
-
 for item in data:
     if isinstance(item, dict):
         keys.extend(item.keys())
         values.extend(item.values())
 
-<<<<<<< HEAD
 print("\nKeys:", keys)
 print("\nValues:", values)
-=======
-#print("Keys:", keys)
-#print("Values:", values)
->>>>>>> cf39b950e8059d9218cc0ff483b4fe70bd7bf1b0
 
 # Find the class key and print the plant's age
 if 'class' in keys:
     class_index = keys.index('class')
-<<<<<<< HEAD
     print("\nYour plant is", values[class_index], "old")
-=======
-    #print("Your plant is", values[class_index], "old")
-
-
-for k in keys: 
-    if k == keys.index('prediction_type'): 
-        index_class = keys.index(k) + 1  
-        new_list = keys[index_class:]
-        print(new_list)
-        
-widths = []
-
-# Loop through the list of keys
-for w, key in enumerate(keys):
-    if key == 'width':
-        # Append the corresponding value to widths list
-        widths.append(values[w])
-
-# Print the extracted widths
-print("Widths:", widths)
-
-
-heights = []
-
-# Loop through the list of keys
-for h, key in enumerate(keys):
-    if key == 'height':
-        # Append the corresponding value to widths list
-        heights.append(values[h])
-
-# Print the extracted widths
-print("Heights:", heights)
-
-max_width = max(widths)
-
-for i in range(len(keys)): 
-    if keys[i] == "width" and values[i] == max_width: 
-        class_index_width = i + (keys[i:].index("class") if "class" in keys[i:] else None)
-        class_value_width = values[class_index]
-        print(class_value_width)
-
-max_height = max(heights)
-for e in range(len(keys)): 
-    if keys[e] == "width" and values[e] == max_height: 
-        class_index_height= e + (keys[e:].index("class") if "class" in keys[e:] else None)
-        class_value_height = values[class_index]
-        print(class_value_height)
-    
-
-
-                
-        
-    #for n in new_list:
-#     # Extract and append width and height values if they exist
-#     if 'width' in n:
-#         widths = [] 
-#         widths.append(n['width'])
-#         print("Widths:", widths)
-#     if 'height' in n:
-#         heights= [] 
-#         heights.append(n['height'])
-#         print("Heights:", heights)
-
-# Print the collected width and height values
-
-
-                
->>>>>>> cf39b950e8059d9218cc0ff483b4fe70bd7bf1b0
 
 # if "class"in keys: 
 #     for i in detections: 
 #         width = keys.index("width")
 #         height = keys.index("height")
 #         boxarea = values[width]*values[height]
-#         print(boxarea) 
-            
-                
-        
-
-# Start the tkinter main loop
-#mainWindow.mainloop()
+#         print(boxarea)
